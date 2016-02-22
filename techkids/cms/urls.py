7b58fda3a106b38be4cf0f65a89ad721d7709d5c
Huy Nguyen <qhuydtvt@gmail.com>
2016-02-22 15:07:35 +0700

    Add cms app

--- techkids/cms/urls.py
+++ techkids/cms/urls.py
@@ -1,10 +1,12 @@
+__author__ = 'qhuydtvt'
+
+from . import views
 from django.conf.urls import include, url
-from django.contrib import admin
 
 urlpatterns = [
     # Examples:
     # url(r'^$', 'techkids.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'^cms/', include('cms.urls')),
-    url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^$', views.index, name="index"),
 ]
---
+++ techkids/cms/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from platform import python_version
+
+# Create your views here.
+
+def index(request):
+    return HttpResponse("Hello, this is TechKids CMS, running on Python" + python_version())
