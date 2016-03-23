1682374aa78fd967ee0e57b5177e3174ddf45b79
Huy Nguyen <qhuydtvt@gmail.com>
2016-03-24 01:23:15 +0700

    Set home page to CMS

--- techkids/techkids/urls.py
+++ techkids/techkids/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     # Examples:
     # url(r'^$', 'techkids.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+    url(r'', include('cms.urls'), name="cms"),
     url(r'^cms/', include('cms.urls'), name="cms"),
     url(r'^admin/', include(admin.site.urls), name="home"),
 ]
